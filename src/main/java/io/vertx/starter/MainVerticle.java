0fd7fe5069d5c6792a071d51dc3f6c905eb35b79
underscorenico <vinuesa.nico@gmail.com>
2017-11-30 22:28:10 +0100

    first commit

---
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -0,0 +1,37 @@
+package io.vertx.starter;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
+import io.vertx.core.Future;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+
+public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+
+    Future<String> dbVerticleDeployment = Future.future();
+    vertx.deployVerticle(new WikiDatabaseVerticle(), dbVerticleDeployment.completer());
+
+    dbVerticleDeployment.compose(id -> {
+
+      Future<String> httpVerticleDeployment = Future.future();
+      vertx.deployVerticle(
+        "io.vertx.guides.wiki.HttpServerVerticle",
+      new DeploymentOptions().setInstances(2),
+      httpVerticleDeployment.completer());
+
+      return httpVerticleDeployment;
+
+    }).setHandler(ar -> {
+      if (ar.succeeded()) {
+        startFuture.complete();
+      } else {
+        startFuture.fail(ar.cause());
+      }
+    });
+  }
+}
